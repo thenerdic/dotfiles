2f8cb98209169c6892d567170b9001abac5a479b
thenerdic <manniemaverick@gmail.com>
2024-02-03 14:55:32 +0700

    installed better comments plugin

--- .config/nvim/lua/thenerdic/packer.lua
+++ .config/nvim/lua/thenerdic/packer.lua
@@ -36,4 +36,5 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+    use "Djancyp/better-comments.nvim"
 end)
